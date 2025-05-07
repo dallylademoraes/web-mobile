52b3e35ccd08aaf984c4ceab93b6adf02f403a0b
dallylademoraes <moraesdallyla6@gmail.com>
2025-05-07 12:35:42 -0300

    page veiculos ready

--- veiculo/views.py
+++ veiculo/views.py
@@ -1,13 +1,26 @@
 from django.shortcuts import render
-
-# Create your views here.
 from veiculo.models import Veiculo
 from django.views.generic import ListView
 
 class ListarVeiculos(ListView):
-  """
-  View para listar veículos cadastrados.
-  """
-  model = Veiculo
-  context_object_name = 'veiculos'
-  template_name = 'veiculo/listar.html'
+    """
+    View para listar veículos cadastrados.
+    """
+    model = Veiculo
+    context_object_name = 'veiculos'
+    template_name = 'veiculo/listar.html'
+
+
+class PesquisarVeiculoView(ListView):
+    """
+    View para pesquisar veículos pelo modelo.
+    """
+    model = Veiculo
+    context_object_name = 'veiculos'
+    template_name = 'veiculo/listar.html'  # Usa o mesmo template da listagem
+
+    def get_queryset(self):
+        termo = self.request.GET.get('q')
+        if termo:
+            return Veiculo.objects.filter(modelo__icontains=termo)
+        return Veiculo.objects.all()
